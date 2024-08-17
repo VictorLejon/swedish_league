19c52b016d02af0ad8d76faafeb9a15a28fe9e9c
Victor Lejon <victor@pou.se>
2024-08-17 23:36:59 +0200

    boiler plate fixed

---
+++ src/components/solid/Test_Counter.jsx
@@ -0,0 +1,12 @@
+import { createSignal } from "solid-js";
+
+function Counter() {
+  const [count, setCount] = createSignal(0);
+  return (
+    <button onClick={() => setCount(count() + 1)}>
+      Count: {count()}
+    </button>
+  );
+}
+
+export default Counter;
