f72cd283ae5a8fe0bf41c6193c69c5797456a194
Victor Lejon <victor@pou.se>
2024-08-17 23:26:50 +0200

    updating boilerplate

---
+++ src/middleware/auth.js
@@ -0,0 +1,6 @@
+// Authentication for page routing
+// Returns: user object || null
+export function authentication(){
+    const user = "UserName_Placeholder" // TODO: Implement auth :)
+    return user || null
+}
